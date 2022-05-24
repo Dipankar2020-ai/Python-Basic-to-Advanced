95059e2026ecb7c8d9a86e13857a2faf992d6a12
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-05-24 21:35:51 +0530

    Create TypeCast And Double It.py

---
+++ Practice Python/TypeCast And Double It.py
@@ -0,0 +1,15 @@
+
+Given an input num as a string. You need to typecast into an integer and double it.
+
+Example 1:
+
+Input:
+num = 5
+Output: 
+10
+Explanation: Just typecast and double it.
+
+Solution:-
+n=input()
+ans=2*int(n)
+print(ans)
